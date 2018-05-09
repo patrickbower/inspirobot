234c23854a766bc54fe74c747487e282125e060b
Patrick Bower <patrickbower@Patricks-MacBook-Pro-2.local>
2018-05-09 23:30:51 +0100

    Conversation rendering
    Add simple styling. Now resembles a chat bot

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Question extends Component {
   render() {
-    return <h2>{this.props.question}</h2>
+    return (
+      <div className="speechbubble-bot">
+        <p>{this.props.question}</p>
+      </div>
+    )
   }
 }
 
