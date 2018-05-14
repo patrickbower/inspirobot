89adb77e6d26082292de0af9ee727574f9514632
Patrick Bower <bower.patrick@gmail.com>
2018-05-14 17:40:48 +0100

    Fiddling around with the UI

--- src/components/NoButton.js
+++ src/components/NoButton.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class YesButton extends Component {
   render() {
-    return this.props.render ? <button type="button" onClick={() => this.props.answer('no')}>No</button> : false;
+    return this.props.render ? <button className="btn btn-primary" type="button" onClick={() => this.props.answer('no')}>No</button> : false;
   }
 }
 
