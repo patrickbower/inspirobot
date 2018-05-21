eed8b899385f08d65440d24e50d7b1a8f717c138
Patrick Bower <bower.patrick@gmail.com>
2018-05-21 18:10:59 +0100

    Firebase setup and running

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import React, { Component } from "react";
 
 // styles
 import "./App.css";
+import firebase from './fire';
 
 // components
 import DialogContainer from "./containers/DialogContainer";
 
 class App extends Component {
+
+    componentWillMount() {
+        var ref = firebase.database().ref();
+        ref.once("value")
+            .then((snap) => {
+                console.log("snap.val()", snap.val());
+            });
+    }
+
     render() {
         return (
             <div>
