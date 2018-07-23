8cfe48802f8ae6c0a87dc8b03c72b8b466599218
Patrick Bower <bower.patrick@gmail.com>
2018-07-23 15:43:12 +0100

    Add suggestion

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,27 +1,33 @@
 import React, { Component } from "react";
 import * as Utils from "../utils/tools";
 import ActionButton from "./ActionButton";
+import firebase from "../fire";
+import * as UserLocalStorage from "../middleware/localStorageApi";
 
 class Action extends Component {
   // saveArticle() {
   //   console.log("Action component - saveArticle()");
   //   return false;
   // }
-  // addSuggestion() {
-  //   console.log("Action component - addSuggestion");
-  //   return false;
-  // }
+
+  addSuggestion() {
+    const itemsRef = firebase.database().ref("suggest");
+    itemsRef.push({ title: UserLocalStorage.get("suggestion") });
+
+    return false;
+  }
 
   getSuggestion() {
     const { suggest } = this.props.data;
     const buttonsArr = [];
+
     for (let key in suggest) {
       buttonsArr.push(
         <ActionButton
           render={true}
           answer={this.props.answer}
           record={this.props.record}
-          text={suggest[key]}
+          text={suggest[key].title}
           key={key}
         />
       );
